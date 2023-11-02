3d0a184fe988dcc7ab6e44aae3feae9218a4223b
CodingNerds900 <149649176+CodingNerds900@users.noreply.github.com>
2023-11-02 16:27:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CODING NERDS</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>CODING NERDS</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="https://drive.google.com/uc?id=1D1cKLHOgxKmKMw0W8YuAcyJkI4ici9xm" target="_blank">PPT</a></li>
+        </ul>
+    </nav>
+    <!-- ... Rest of your HTML content ... -->
+    
+    <script>
+        // JavaScript code to display an alert when the page loads
+        window.onload = function() {
+            alert('Welcome to the Coding Nerds website!');
+        }
+    </script>
+    
+    <!-- ... Rest of your HTML content ... -->
+
+    <footer>
+        <p>&copy; CODING NERDS</p>
+    </footer>
+</body>
+</html>
